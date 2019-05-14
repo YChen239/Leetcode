bc062cfd77aa53242d89976d7c4209891c0a7ea9
Yuhan Chen <cyhhanna097@gmail.com>
2019-05-14 15:51:23 -0400

    Update AddTwoNumbers.py

--- LinkedList/AddTwoNumbers.py
+++ LinkedList/AddTwoNumbers.py
@@ -21,3 +21,16 @@ class Solution(object):
             carry = carry//10
             
         return dummy.next
+    
+#Convert to int and reconvert to list
+#64s/12MB
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        def toint(node):
+            return node.val + 10 * toint(node.next) if node else 0
+        def tolist(n):
+            node = ListNode(n % 10)
+            if n > 9:
+                node.next = tolist(n / 10)
+            return node
+        return tolist(toint(l1) + toint(l2))
